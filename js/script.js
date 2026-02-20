36481b3a389ad8f6d366cb0c02b99751c5a8154e
Rosa <rosajuliapena30@gmail.com>
2026-02-19 23:25:55 -0400

    js correjido

--- js/script.js
+++ js/script.js
@@ -1,14 +1,13 @@
-
-
 const hora = document.getElementById("hora");
 const minuto = document.getElementById("minuto");
 const segundo = document.getElementById("segundo");
 
 const horaDigital = document.getElementById("horaDigital");
+const fechaTexto = document.getElementById("fechaTexto");
+const diaNumero = document.getElementById("diaNumero");
 
 const botonModo = document.getElementById("botonModo");
 const cuerpo = document.getElementById("cuerpo");
-
 
 function actualizarReloj() {
 
@@ -18,7 +17,6 @@ function actualizarReloj() {
     const minutos = ahora.getMinutes();
     const segundos = ahora.getSeconds();
 
-
     const gradosHora = (horas % 12) * 30 + minutos * 0.5;
     const gradosMinuto = minutos * 6;
     const gradosSegundo = segundos * 6;
@@ -27,50 +25,32 @@ function actualizarReloj() {
     minuto.style.transform = `translateX(-50%) rotate(${gradosMinuto}deg)`;
     segundo.style.transform = `translateX(-50%) rotate(${gradosSegundo}deg)`;
 
+ 
     let formato = horas >= 12 ? "PM" : "AM";
-    let horaMostrar = horas % 12;
-    horaMostrar = horaMostrar ? horaMostrar : 12;
-
+    let horaMostrar = horas % 12 || 12;
     let minutosFormato = minutos.toString().padStart(2, "0");
 
+    horaDigital.textContent = `${horaMostrar}:${minutosFormato} ${formato}`;
 
-    const dias = [
-        "DOMINGO","LUNES","MARTES","MIÉRCOLES",
-        "JUEVES","VIERNES","SÁBADO"
-    ];
-
-    const meses = [
-        "ENE","FEB","MAR","ABR","MAY","JUN",
-        "JUL","AGO","SEP","OCT","NOV","DIC"
-    ];
+    const dias = ["DOMINGO","LUNES","MARTES","MIÉRCOLES","JUEVES","VIERNES","SÁBADO"];
+    const meses = ["ENE","FEB","MAR","ABR","MAY","JUN","JUL","AGO","SEP","OCT","NOV","DIC"];
 
     let diaTexto = dias[ahora.getDay()];
     let mesTexto = meses[ahora.getMonth()];
     let numeroDia = ahora.getDate();
 
-
-    horaDigital.innerHTML = `
-        ${horaMostrar}:${minutosFormato} ${formato}
-        <span class="fecha-inline">
-            ${diaTexto}, ${mesTexto}
-            <span class="dia-circulo">${numeroDia}</span>
-        </span>
-    `;
+    fechaTexto.textContent = `${diaTexto}, ${mesTexto}`;
+    diaNumero.textContent = numeroDia;
 }
-
 
 setInterval(actualizarReloj, 1000);
 actualizarReloj();
 
-
 botonModo.addEventListener("click", () => {
-
     cuerpo.classList.toggle("oscuro");
 
-    if (cuerpo.classList.contains("oscuro")) {
-        botonModo.textContent = "Modo claro";
-    } else {
-        botonModo.textContent = "Modo oscuro";
-    }
-
+    botonModo.textContent = 
+        cuerpo.classList.contains("oscuro") 
+        ? "Modo claro" 
+        : "Modo oscuro";
 });
